d3359e06f1dd88f4cf29613cf0ff5de8a2fe6ee5
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2023-06-05 10:01:13 +0800

    Modify loop include width height to fit blocks in sketchArea

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 const sketchArea = document.querySelector("#sketchArea");
 // const block = document.createElement("div");
-let limit = 10;
+let limit = 100;
 
 // block.classList.add("block");
 // block.textContent = "1";
 
-for(i = 0; i < limit; i++){
+
+
+
+
+for(let i = 0; i < limit * limit; i++){
     const block = document.createElement("div");
     block.classList.add("block");
     // block.textContent = "";
+    block.style.width = `${640 / limit}px`;
+    block.style.height = `${640 / limit}px`;
     sketchArea.appendChild(block);
-    for(j = 0; j < limit; j++){
-        const block = document.createElement("div");
-        block.classList.add("block");
-        // block.textContent = "";
-        sketchArea.appendChild(block);
-    }
+    // for(let j = 0; j < limit; j++){
+    //     const block = document.createElement("div");
+    //     block.classList.add("block");
+    //     block.textContent = "";
+    //     sketchArea.appendChild(block);
+    // }
 }
 
 
+//find out how to select newly created divs in loop
+const blocks = document.querySelectorAll(".block");
+
+blocks.forEach(blocks => blocks.addEventListener("mouseover", () => {
+    blocks.style.background = "green";
+    
+}));
 
 
 // sketchArea.appendChild(block);
