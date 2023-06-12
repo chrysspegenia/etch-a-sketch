cc0e8d09ae0986830c2ad76422caf80c63151a2d
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2023-06-12 15:58:56 +0800

    Add btn variables and effects when a mode is selected

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const clearBtn = document.getElementById("clear");
 const slideNum = document.querySelector("#slideNumDisplay");
 const inputSlider = document.getElementById("inputSlider");
 const inputColor = document.getElementById("colorPicker");
+const buttons = document.querySelectorAll("button");
+const colorBtn = document.getElementById("color");
+const rgbBtn = document.getElementById("rgb");
+const eraserBtn = document.getElementById("eraser");
 let modeSelection;
 
 inputSlider.oninput = (() => {
@@ -27,6 +31,7 @@ function grid(){
     for(let i = 0; i < gridCount * gridCount; i++){
         const sketchBlock = document.createElement("div");
         sketchBlock.classList.add("sketchBlock");
+        sketchBlock.setAttribute("draggable", false);//doesnt fully solve drag
         sketchBlock.style.width = `${640 / gridCount}px`;
         sketchBlock.style.height = `${640 / gridCount}px`;
         sketchArea.appendChild(sketchBlock);
@@ -34,11 +39,11 @@ function grid(){
 };
 grid()
 
-const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => button.addEventListener("click", () => {
     modeSelection = button.id;
     colorMode();
+    buttonModeColor();
 }));
 
 
@@ -82,4 +87,22 @@ inputColor.oninput = (() => {
 });
 
 
-
+function buttonModeColor(){
+    if(modeSelection == "color"){
+        colorBtn.classList.replace("button","activeBtn");
+        eraserBtn.classList.replace("activeBtn","button");
+        rgbBtn.classList.replace("activeBtn","button");
+    } else if (modeSelection == "eraser"){
+        eraserBtn.classList.replace("button","activeBtn");
+        colorBtn.classList.replace("activeBtn","button");
+        rgbBtn.classList.replace("activeBtn","button");
+    } else if (modeSelection == "rgb"){
+        rgbBtn.classList.replace("button","activeBtn");
+        colorBtn.classList.replace("activeBtn","button");
+        eraserBtn.classList.replace("activeBtn","button");
+    } else {
+        colorBtn.classList.replace("activeBtn","button");
+        eraserBtn.classList.replace("activeBtn","button");
+        rgbBtn.classList.replace("activeBtn","button");
+    }
+};
